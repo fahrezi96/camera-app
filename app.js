521f1678bc9d70ddee88a500f1a2d0df58cf5b28
fahrezi96 <35027944+fahrezi96@users.noreply.github.com>
2020-08-14 17:12:53 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let constraints = {
+  video: { facingMode: "user" },
+  audio: false,
+};
+
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+
+function cameraStart() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error("Oops. Something is broken.", error);
+    });
+}
+
+cameraTrigger.onclick = () => {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+};
+
+window.addEventListener("load", cameraStart, false);
